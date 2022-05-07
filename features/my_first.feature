ac107b0dccb8223d54e50a329517aa0f875e83fe
Luis Guzman <lm.guzmanp1@uniandes.edu.co>
2022-05-07 14:48:59 -0500

    Create a_create-page.feature script
    - Improve package.json scripts
    - Create step definitions for first scenario
    
    Signed-off-by: Luis Guzman <lm.guzmanp1@uniandes.edu.co>

--- features/my_first.feature
+++
@@ -1,6 +0,0 @@
-Feature: My feature
-
-  @user1 @web
-  Scenario: My scenario 1
-    Given I wait
-    When I send a signal to user 2 containing "hi"
--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -1,1 +1,23 @@
 const {Given, When, Then} = require('@cucumber/cucumber');
+
+Given('I navigate to page "{kraken-string}"', function (page) {
+    return this.driver.url(page);
+});
+
+Then('I wait for {kraken-int} seconds', function (int) {
+    return this.driver.setTimeout(int);
+});
+
+When('I click in {kraken-string}', async function (string) {
+    const element = await this.driver.$(`${string}`);
+    return element.click();
+});
+
+When('I type {kraken-string} into {kraken-string}', async function (stringToType, field) {
+    const input = await this.driver.$(field);
+    await input.setValue(stringToType);
+});
+
+Then('I take a screenshot as evidence {kraken-string}', async function (string) {
+    await this.driver.saveScreenshot(`./${string}.png`);
+});
